3339f0d841c87b86f1767e21979fde787298c2a6
Leiver Moreno <leivermoreno@gmail.com>
2025-03-04 10:49:22 -0500

    add util func to ensure ids are valid ints

--- todo_app/utils.py
+++ todo_app/utils.py
@@ -4,7 +4,15 @@ from db import db
 from models import Project, Task
 
 
+def to_int(value):
+    try:
+        return int(value)
+    except ValueError:
+        abort(404)
+
+
 def get_project(project_id):
+    project_id = to_int(project_id)
     project = db.session.get(Project, project_id)
     if project is None or project.user_id != g.user.id:
         abort(404)
@@ -13,6 +21,7 @@ def get_project(project_id):
 
 
 def get_task(task_id):
+    task_id = to_int(task_id)
     task = db.session.get(Task, task_id)
     if task is None or task.project.user_id != g.user.id:
         abort(404)
